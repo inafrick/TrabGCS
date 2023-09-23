79f5e225a0613a3a826460be1ff3eb32e3d23ef2
inafrick <inaciopimentel7@gmail.com>
2023-09-23 19:56:59 -0300

    feat: adicionei os departamentos

--- Empresa.java
+++ Empresa.java
@@ -1,17 +1,34 @@
+import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Empresa {
+    private Scanner entrada;
     private ArrayList<Funcionario> funcionarios;
     private ArrayList<RegistroCusto> registros;
+    private ArrayList<String> departamentos;
 
     public Empresa(){
+        entrada = new Scanner(System.in);
         funcionarios = new ArrayList<>();
         registros = new ArrayList<>();
+        departamentos = new ArrayList<>();
     }
 
     public void executa(){
-        return;
+        criaDepartamentos();
+        login();
+    }
 
+    private void criaDepartamentos(){
+        departamentos.add("RH");
+        departamentos.add("Compras");
+        departamentos.add("Vendas");
+        departamentos.add("Expedição");
+        departamentos.add("Engenharia");
+        departamentos.add("TI");
+        departamentos.add("Produção");
+        departamentos.add("Marketing");
+        departamentos.add("Financeiro");
     }
 
     private void login(){
