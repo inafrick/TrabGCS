a35cf6e7c108ff164ad5d41f9f7585da1557cb39
Murilo Machado <md.murilomachado@gmail.com>
2023-09-23 21:34:48 -0300

    feat: cadastra cliente e menu teste passo 2

--- Empresa.java
+++ Empresa.java
@@ -17,6 +17,7 @@ public class Empresa {
     public void executa(){
         criaDepartamentos();
         login();
+        executaMenu();
     }
 
     private void criaDepartamentos(){
@@ -34,10 +35,63 @@ public class Empresa {
     private void login(){
 
     }
+    
+    private void executaMenu(){
+        int op;
+        do{
+            menu();
+            System.out.println("\nDigite a opção desejada: ");
+            op = entrada.nextInt();
+            entrada.nextLine();
+            switch(op){
+                case 0: 
+                    break;
+                case 2: 
+                    cadastraFuncionario();
+            }
+        }   while(op!=0);
+    }
+
+    private void menu(){
+        System.out.println("============= MENU =============");
+		System.out.println("[2] Cadastra Funcionário");
+    }
 
     private void cadastraFuncionario(){
+        int matricula;
+        String nome;
+        String departamento;
 
-    }
+        System.out.println("\nOpção Selecionada: [2] Cadastra Funcionário.");
+        System.out.println("Informe a matrícula do funcionário: ");
+        matricula = entrada.nextInt();
+
+        boolean matriculaExistente = false;
+        for(Funcionario f : funcionarios){
+            if(matricula==f.getMatricula()){
+                matriculaExistente = true;
+                System.out.println("Erro. Matrícula já pertence a um funcionário cadastrado.");
+                break;
+            }        
+        }
+
+        if(!matriculaExistente){
+            entrada.nextLine();
+            System.out.println("Informe o nome do funcionário: ");
+            nome = entrada.nextLine();
+            System.out.println("Informe o departamento do funcionário: ");
+            departamento = entrada.nextLine();
+                if(!departamentos.contains(departamento)){
+                    System.out.println("Departamento não encontrado. Funcionário não cadastrado.");
+                }   
+                else{
+                    Funcionario funcionario = new Funcionario(matricula, nome, departamento);
+                    funcionarios.add(funcionario);
+                    System.out.println("Funcionário cadastrado. \nMatrícula: "+matricula+" | Nome: "+nome+" | Departamento: " + departamento);
+                }
+            }
+        }
+
 
     private void pesquisaRegistroCusto(){
 
